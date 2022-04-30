5472f9674a767c839faab5f6dbd95f4f069bd0d9
AWalf43 <michaelmei222@yahoo.com.au>
2022-04-30 15:02:33 +1000

    Added box classes

---
+++ Project/boxes.py
@@ -0,0 +1,18 @@
+# Box dimensions in centimetres
+
+class LargeBox:
+    length: 100
+    width: 100
+    depth: 40
+
+
+class MediumBox:
+    length: 75
+    width: 75
+    depth: 40
+
+
+class SmallBox:
+    length: 40
+    width: 40
+    depth: 40
--- Project/objecttypes.py
+++ Project/objecttypes.py
@@ -1,3 +1,8 @@
+# Dimensions in centimetres
+# dm types can be placed anywhere
+# f types require their own padded box
+# t types require to be placed on the topmost layers
+
 class Apples:
     length = 5
     width = 5
@@ -5,5 +10,15 @@ class Apples:
     type = "dm"
 
 
-test = Apples()
-print(test.type)
+class Bottles:
+    length = 5
+    width = 5
+    height = 15
+    type = "f"
+
+
+class Eggs:
+    length = 10
+    width = 5
+    height = 5
+    type = "t"
