35da8efc558f8dd04f8d628580c780ec98406ef9
AWalf43 <michaelmei222@yahoo.com.au>
2022-04-30 23:20:13 +1000

    Added try except statement to handle invalid item names within input csv

--- Project/Sources/boxalgorithm.py
+++ Project/Sources/boxalgorithm.py
@@ -1,29 +1,32 @@
 import csv
+import sys
 from Includes import objecttypes as obj
 from Includes import boxes as box
+
+
+# Function to get the volume of an input object for use with later sorting
+def vol(entry):
+    return entry.volume
+
 
 with open('../Includes/items/itemList.csv', newline='') as csvfile:
     contents = csv.reader(csvfile, delimiter=',', quotechar='|')
     for line in contents:
+        # array to store input items
+        everything = []
+
         for i in line:
             # Compares content of csv file to dictionary containing all item names to create objects
-            print(obj.allItems[i.lower()]().name)
+            # print(obj.allItems[i.lower()]().name)
+            try:
+                everything.append(obj.allItems[i.lower()]())
+            except KeyError:
+                sys.stderr.write("Invalid item name")
+                exit(1)
 
-# scan = "Eggs"
-# x = obj.allItems[scan]()
-# print(x.name)
+        everything.sort(key=vol)
+        for i in range(len(everything)):
+            print(everything[i].name)
+        print("")
 
-# pear = obj.Apples()
-# print(pear.length)
-#
-# firstbox = box.LargeBox()
-# print(firstbox.length)
-#
-# objects = []
-#
-# for x in range(10):
-#     item = obj.Apples()
-#
-#     objects.append(item)
-#
-# print(objects[1].name)
+
