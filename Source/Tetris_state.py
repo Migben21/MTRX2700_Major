28d8d3c5f6fee4ec6ea3923475399531ed6fc43c
Pabso <yogpast45@gmail.com>
2022-05-01 21:51:18 +1000

    Version 0.01

--- Source/Tetris_state.py
+++ Source/Tetris_state.py
@@ -2,7 +2,21 @@ import numpy as np
 import matplotlib as mp
 
 def tetris_main(items, boxes):
+    #array of boxes in line with items 
+    #compare name of box with the first element in the items array to generate first box to be created
 
+    
+    #Make 3D matrix for each box
+    box_matrix = []
+
+    for i in range(box.length/5):
+        box_matrix.append([])
+
+        for j in range(box.width/5):
+            box_matrix[i].append([])
+
+            for k in range(box.hight/5):
+                box_matrix[i][j].append([])
 
 
 
