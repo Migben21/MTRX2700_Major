7d9d297fff675cf61121cd847005c166d808139e
AWalf43 <michaelmei222@yahoo.com.au>
2022-05-02 20:27:50 +1000

    Created SortedBox class

--- Project/Includes/boxes.py
+++ Project/Includes/boxes.py
@@ -6,6 +6,7 @@ class LargeBox:
         self.width = 100
         self.depth = 40
         self.volume = self.length * self.width * self.depth
+        self.type = 'l'
 
 
 class MediumBox:
@@ -14,6 +15,7 @@ class MediumBox:
         self.width = 75
         self.depth = 40
         self.volume = self.length * self.width * self.depth
+        self.type = 'm'
 
 
 class SmallBox:
@@ -22,4 +24,27 @@ class SmallBox:
         self.width = 40
         self.depth = 40
         self.volume = self.length * self.width * self.depth
+        self.type = 's'
 
+
+boxSizes = {'s': SmallBox, 'm': MediumBox, 'l': LargeBox}
+
+
+class SortedBox:
+    def __init__(self, size):
+        self.contents = []
+        self.type = size
+
+        match size:
+            case 's':
+                self.length = 40
+                self.width = 40
+                self.depth = 40
+            case 'm':
+                self.length = 75
+                self.width = 75
+                self.depth = 40
+            case 'l':
+                self.length = 100
+                self.width = 100
+                self.depth = 40
--- Project/Sources/boxalgorithm.py
+++ Project/Sources/boxalgorithm.py
@@ -4,9 +4,15 @@ from Includes import objecttypes as obj
 from Includes import boxes as box
 
 
+# Functions to be used for .sort() key parameter later
 # Function to get the volume of an input object for use with later sorting
 def vol(entry):
     return entry.volume
+
+
+# Function to return the type of the input object
+def get_type(entry):
+    return entry.type
 
 
 with open('../Includes/items/itemList.csv', newline='') as csvfile:
@@ -27,7 +33,5 @@ with open('../Includes/items/itemList.csv', newline='') as csvfile:
         # Sorts array in order from largest to smallest
         everything.sort(key=vol, reverse=True)
         for i in range(len(everything)):
-            print(everything[i].name)
+            print(everything[i].name + ' ' + str(everything[i].volume))
         print("")
-
-
