f29a8e656d19cab12cc250fb36abede02af8c07b
Pabso <yogpast45@gmail.com>
2022-05-02 18:12:49 +1000

    version 0.11 update to 3D matrix Generator

--- Source/Tetris_state.py
+++ Source/Tetris_state.py
@@ -1,22 +1,36 @@
 import numpy as np
 import matplotlib as mp
+from Includes import boxes as box
 
-def tetris_main(items, boxes):
-    #array of boxes in line with items 
-    #compare name of box with the first element in the items array to generate first box to be created
+def tetris_main(items_multi):
+    #determine amount of boxes and set to array
+    for b in range(items_multi ): #set to change
 
-    
-    #Make 3D matrix for each box
-    box_matrix = []
+        items = items_multi[b]
 
-    for i in range(box.length/5):
-        box_matrix.append([])
+        #array of boxes in line with items 
+        x,y,z = 0
+        boxes = [box.SmallBox, box.MediumBox, box.LargeBox]
+        #compare name of box with the first element in the items array to generate first box to be created
 
-        for j in range(box.width/5):
-            box_matrix[i].append([])
+        for i in range(1,4):
+            if items[0] == i:
+                y = boxes[i].length
+                x = boxes[i].width
+                z = boxes[i].height
+                break
 
-            for k in range(box.hight/5):
-                box_matrix[i][j].append([])
+        #Make 3D matrix for each box
+        box_matrix = []
+
+        for i in range(x/5):
+           box_matrix.append([])
+
+           for j in range(y/5):
+                box_matrix[i].append([])
+
+                for k in range(z/5):
+                   box_matrix[i][j].append([])
 
 
 
