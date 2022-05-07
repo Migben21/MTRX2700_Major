83cd35721f54750d29cec5d22cfff4d0cc505375
scap7758 <scap7758@uni.sydney.edu.au>
2022-05-07 22:06:48 +1000

    Update 1.31

--- Project/Source/Tetris_state.py
+++ Project/Source/Tetris_state.py
@@ -1,5 +1,7 @@
+from msilib.schema import Error
 import numpy as np
 import matplotlib as mp
+from sympy import Point, Point3D, true
 from Includes import boxes as box
 
 class item_organised:
@@ -14,43 +16,62 @@ def tetris_init(box_objs):
     #determine amount of boxes and set to array
     for b in range(box_objs): #set to change
 
+        #box data
         box = box_objs[b]
-
-        #array of boxes in line with items 
-        x = box.width
-        y = box.length
-        z = box.height
+        dimentions = [box.width/5, box.length/5, box.height/5 ]
 
         #Make 3D matrix for each box
         box_matrix = []
 
-        for i in range(x/5):
+        for i in range(dimentions[0]):
            box_matrix.append([])
 
-           for j in range(y/5):
+           for j in range(dimentions[1]):
                 box_matrix[i].append([])
 
-                for k in range(z/5):
+                for k in range(dimentions[2]):
                    box_matrix[i][j].append([])
 
         #begin organising objects in the box
-        tetris_main(box_matrix, box.conents)
+        box_pos_data = tetris_main(box_matrix, box.conents, dimentions)
     
+
+
+def tetris_main(box_matrix, contents, dimentions):
+    item_pos_data = []
+
+    for i in range(contents):
+        obj_dataframe = ["",[0,0,0],[0,0,0],[0,0,0],0] #[name, (point_x, point_y, point_z), (length, width, hight), (rotation x_axis, rotation y_axis rotation z_axis)]
+
+    #set name and length, width and hight  
+
+    #snaking algorithm for each item in the box
+        for z in range(0, dimentions[0]+1):
+           for y in range(0, dimentions[1]+1):
+                for x in range(0, dimentions[2]+1):
+                    space_check(box_matrix, obj_dataframe) #double check that obj_dataframe and box_matrix can be sent back and forth 
+                    
+                    if obj_dataframe[4] == 1:
+                        item_pos_data.append(obj_dataframe)
+                        break
+                    if obj_dataframe[4] == -1:
+                        return Error
+
+
+
+
+
+                    
+
+                   
 
 
 
 ##to be used later
-def tetris_main(box_matrix, contents):
 
 def tetris_place_item():
 
 def tetris_item_coordinates():
-
-def move_y_test():
-
-def move_x_test():
-
-def move_z_test():
 
 def orientation_test():
 
