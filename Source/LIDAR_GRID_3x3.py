649c073120fbbfdb084c8f8214c54fdff745814a
Migben21 <benatiro.miguel@gmail.com>
2022-05-18 04:35:11 +1000

    Updated 3x3 Grid Code for LIDAR

--- Source/LIDAR_GRID_3x3.py
+++ Source/LIDAR_GRID_3x3.py
@@ -8,33 +8,80 @@ lvl_1 = 3
 lvl_2 = 6
 lvl_3 = 9
 
-def compareValue(value):
-    value = np.abs(value)
-    if (value > lvl_0):
-        gap = lvl_1 - lvl_0
-        value - gap/2
-        closest_value = lvl_0
+layer_1 = []
+layer_2 = []
+layer_3 = []
 
-    elif (value > lvl_1):
-        gap = lvl_2 - lvl_1
-        closest_value = lvl_1
+i = 0
+while i < 9:
+    layer_1.append(0)
+    layer_2.append(0)
+    layer_3.append(0)
+    i+=1
+
+print("Layer 1:", end = ' ') 
+print(layer_1)
+print("Layer 2:", end = ' ')
+print(layer_2)
+print("Layer 3:", end = ' ')
+print(layer_3)
+
+'''
+def compareValue(value):
+    #value = np.abs(value)  # This is only if you want it to take in integers
+    
+    if (value > lvl_3):
+        closest_value = lvl_3
 
     elif (value > lvl_2):
-        gap = lvl_2 - lvl_3
+        if value > ((lvl_2-lvl_1)/2):
         closest_value = lvl_2
-
-    elif (value > lvl_3):
-        gap = lvl_2 - lvl_3
+        else:
         closest_value = lvl_3
+
+    elif (value > lvl_1):
+        gap = lvl_1 - lvl_0
+        if value > ((lvl_1-lvl_0)/2):
+        closest_value = lvl_1
+        else:
+        closest_value = lvl_2
+        
+    elif (value > lvl_0):
+        if value > ((lvl_1-lvl_0)/2):
+        closest_value = lvl_0
+        else:
+        closest_value = lvl_1
 
     else:
         print("Invalid value.")
     
     return closest_value
+'''
 
-# 1 Array for each layer?
-layer_1 = []
 
+
+'''
+# Creating Layer Arrays
+no_of_layers = 3
+area = 9
+i = 1
+layer_ = [] # Multiple dimension array; an array that will contain the layer arrays
+while i <= no_of_layers:
+    layer_[i] = [] # Creates an array for each layer
+
+    j = 0
+    while j < area:
+        layer_[i].append(0)
+        j+=1
+
+    i+=1
+
+i = 0
+while i <= no_of_layers:
+    print("Layer:", end = ' ')
+    print(layer_[i])
+    i+=1
+'''
 
 '''
 def find_nearest(array, value):
