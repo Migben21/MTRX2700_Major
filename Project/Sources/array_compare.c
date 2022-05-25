457bc24ae122eeb167971130ccccdf886db9b27a
Migben21 <benatiro.miguel@gmail.com>
2022-05-25 14:51:57 +1000

    Trying to upload compare array

--- Project/Sources/array_compare.c
+++ Project/Sources/array_compare.c
@@ -15,17 +15,17 @@ int main() {
     //int points[] = {1, 3, 4, 9, 19, 20}; // Points to compare
     int set_1[27] = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37};
     int set_2[27] = {11, 00, 13, 00, 00, 16, 17, 18, 00, 20, 21, 22, 23, 00, 25, 26, 27, 28, 00, 30, 31, 32, 00, 34, 35, 00, 00};
-    int array[27];
+    int invalid_array[27];
 
     int i = 0;
     for (i=0; i < 27; i++) {
-        point_compare(set_1[i], set_2[i], &array[i], i);
+        point_compare(set_1[i], set_2[i], &invalid_array[i], i);
     }
 
     printf("Invalid point indexi: ");
     for (i=0; i < 27; i++) {
-        if (array[i] != -1) {
-            printf("%i ", array[i]);
+        if (invalid_array[i] != -1) {
+            printf("%i ", invalid_array[i]);
         }
         //printf("%i ", array[i]);
     }
