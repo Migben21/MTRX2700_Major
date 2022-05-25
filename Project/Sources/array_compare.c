8985feb70da2a609bfaa5b3a62c6de5ffd2aefd4
Migben21 <benatiro.miguel@gmail.com>
2022-05-25 14:22:25 +1000

    array function changes

--- Project/Sources/array_compare.c
+++ Project/Sources/array_compare.c
@@ -15,7 +15,6 @@ int main() {
     //int points[] = {1, 3, 4, 9, 19, 20}; // Points to compare
     int set_1[27] = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37};
     int set_2[27] = {11, 00, 13, 00, 00, 16, 17, 18, 00, 20, 21, 22, 23, 00, 25, 26, 27, 28, 00, 30, 31, 32, 00, 34, 35, 00, 00};
-
     int array[27];
 
     int i = 0;
@@ -34,20 +33,7 @@ int main() {
     return 0;
 }
 
-/*
-arr 1
-arr 2
-points
-inc array
-void single_compare(int data1, int data2, int *storage_array_entry);
-    *storage_array_entry = result;
- function thats take the address of a single int in the array and modifies it
- for(int i = 0; i < 27; i++){
-     single_compare(set_1[i], set_2[i], &array[i]);   
- }
- */
-
-/*
+/* Old attempt that was meant to take in and output arrays
 int array_compare(int comp_points[], int dataset_1[], int dataset_2[], int *invalid_points[]) {
     int i = 0, k = 0, j = 0;
     
