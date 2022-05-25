5fc69f57bde98a48ec16c53aa6a7daade7323a1f
AWalf43 <mmei4086@uni.sydney.edu.au>
2022-05-25 15:16:49 +1000

    Added array compare files

--- Project/Sources/array_compare.c
+++ Project/Sources/array_compare.c
@@ -1,58 +1,25 @@
 #include <stdio.h>
+#include "array_compare.h"
+
+void point_compare(int data_1, int data_2, int *storage_array, int counter) {
+    if (data_1 != data_2) {
+        *storage_array = counter;
+    }
+    else{
+        *storage_array = -1;
+    }
+}
 
 int array_compare() {
-    // Hardcoded Input for now:
-    int comp_points[] = {2, 4, 5, 6}; // Points to compare
-    int dataset_1[] = {14, 32, 23, 55, 66, 34, 50};
-    int dataset_2[] = {14, 32, 23, 43, 40, 34, 50};
+    // Hardcoded Input for now
+    //int points[] = {1, 3, 4, 9, 19, 20}; // Points to compare
+    int set_1[27] = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37};
+    int set_2[27] = {11, 00, 13, 00, 00, 16, 17, 18, 00, 20, 21, 22, 23, 00, 25, 26, 27, 28, 00, 30, 31, 32, 00, 34, 35, 00, 00};
+    int invalid_array[27];
 
-    int i, k, j = 0;
-    int invalid_points[69]; // Array to store index numbers of invalid points
-    int no_invalid = 0; // Integer to store number of invalid points
-
-    // Checking sizes of arrays, division to account for memory differences
-    // Checking number of points to compare
-    int no_comp_points = (sizeof comp_points) / (sizeof comp_points[0]); 
-    
-    int no_points_1 = (sizeof dataset_1) / (sizeof dataset_1[0]);
-    int no_points_2 = (sizeof dataset_2) / (sizeof dataset_2[0]);
-    if (no_points_1 != no_points_2) {
-        printf("Datasets do not have same number of points.\n");
+    int i = 0;
+    for (i=0; i < 27; i++) {
+        point_compare(set_1[i], set_2[i], &invalid_array[i], i);
     }
-
-    printf("Points to Compare: ");
-    for (i=0; i < no_comp_points; i++) {
-        printf("%d ", comp_points[i]);
-    }
-
-    for (i=0; i < no_points_1; i++) {
-        // Checks if it's one of the points to be compared, if so then compares
-        // Have to put -1 since i starts counting from 0
-        if (i == comp_points[k] - 1) { 
-            if (dataset_1[i] != dataset_2[i]) {
-                invalid_points[j] = i + 1; // Since i counts at 0
-                j++;
-                no_invalid++;
-            }
-            k++;
-        }
-    }
-
-/* // This variation checks through all the points
-    for (i=0; i < no_points_1; i++) {
-        if (dataset_1[i] != dataset_2[i]) {
-            invalid_points[j] = i;
-            j++;
-            no_invalid++;
-        }
-    }
-*/
-
-    printf("\nInvalid points: ");
-    for (i=0; i < no_invalid; i++) {
-        printf("%d ", invalid_points[i]);
-    }
-    printf("\n");
-
-    return 0;
 }
+
